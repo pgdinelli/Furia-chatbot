7512edb0f75e4ffa953d910b1f23f45b932db903
dinellipaulo <pgsdinelli@gmail.com>
2025-04-25 12:51:30 -0400

    scripts funcionais implementados

---
+++ frontend/scripts.js
@@ -0,0 +1,21 @@
+async function sendMessage() {
+    // instanciando os objetos de inout e chat
+    const userInput = document.getElementById("user-input");
+    const userMessage = userInput.value;
+
+    const chatBox = document.getElementById("chat-box");
+    chatBox.innerHTML += `<div class='user'><strong>Você:</strong> ${userMessage}</div>`;
+    userInput.value = "";
+
+    // fazendo a conexão com a API do backend
+    const response = await fetch("/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userMessage })
+      });
+
+    const data = await response.json();
+    chatBox.innerHTML += `<div class='bot'><strong>FURIAbot:</strong> ${data.reply}</div>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
+
+}
