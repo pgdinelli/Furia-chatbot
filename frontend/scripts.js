5878c235bc6f5e913f130123d202390368b810b0
dinellipaulo <pgsdinelli@gmail.com>
2025-04-28 16:08:20 -0400

    testando backend local com produção

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -27,7 +27,7 @@ async function sendMessage() {
 
 
   // fazendo a conexão com a API do backend
-  const response = await fetch("http://127.0.0.1:5000", {
+  const response = await fetch("http://127.0.0.1:5000/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: userMessage })
